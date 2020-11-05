e497724ef75a250797d832f1c52c760502751413
Adam Preston <adam.m.preston@outlook.com>
2020-11-05 06:54:22 -0700

    add logging for when the file size is too small

--- app/jobs/snapshot_worker.rb
+++ app/jobs/snapshot_worker.rb
@@ -9,6 +9,9 @@ class SnapshotWorker
       if snapshot.forecast_web_file_size > 500
         snapshot.save!
         SnapshotChangeWorker.perform_async snapshot.id 
+      else
+        logger.info "The snapshot file size is #{snapshot.forecast_web_file_size} and thats too small"
+      
       end
 
     end
